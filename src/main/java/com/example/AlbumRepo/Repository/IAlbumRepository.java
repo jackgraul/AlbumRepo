ed2091e6fcd5bbb64818d604a5b7635dfb1d8ce7
jackgraul <158768439+jackgraul@users.noreply.github.com>
2025-10-21 00:18:41 -0400

    Updates to CoverArtService. Assigns cover url to all currently but from manually checking there are many wrong covers

--- src/main/java/com/example/AlbumRepo/Repository/IAlbumRepository.java
+++ src/main/java/com/example/AlbumRepo/Repository/IAlbumRepository.java
@@ -3,10 +3,14 @@ package com.example.AlbumRepo.Repository;
 import com.example.AlbumRepo.Entity.Album;
 import com.example.AlbumRepo.Entity.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface IAlbumRepository extends JpaRepository<Album,Integer> {
+    @Query("SELECT a FROM Album a JOIN FETCH a.artist WHERE a.coverURL IS NULL OR a.coverURL = '' OR a.coverURL LIKE CONCAT('%', '/images/default-cover.png', '%') OR a.coverURL LIKE CONCAT('%', 'spacer.gif', '%')")
+    List<Album> findAllWithoutCovers();
+
     // Find all albums by artist
     List<Album> findByArtist(Artist artist);
 
